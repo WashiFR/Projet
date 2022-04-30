b291efe3259ff325aafcc17e56f69c0d0584467c
Washi <50027934+WashiFR@users.noreply.github.com>
2022-04-30 12:06:06 +0200

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,11 +1,11 @@
-from importlib import import_module
 import os
 import random
 
 def jeu_wordle():
     trop = None
-    nb_tentatives = 10
-    liste_mots = ["test"]
+    nb_tentatives = 7
+    liste_mots = ['ABATS','ABIME','ABOIE','ACCRO','ADMIT']
+    liste_mauvais_mots = []
     bon_mot = liste_mots[random.randint(0, len(liste_mots)-1)]
     liste_bon_mot = [i for i in bon_mot]
     liste = []
@@ -23,9 +23,9 @@ def jeu_wordle():
             print("\033[1;37mTrop de caractères !\033[0;0m")
         elif trop == False:
             print("\033[1;37mPas assez de caractères !\033[0;0m")
-        print("\033[1;37mNombres de tentatives :",nb_tentatives,"\033[0;0m\n\n\33[1;37m"+''.join(liste),'\033[0;0m')
+        print("\033[1;37mNombres de tentatives :",nb_tentatives,"\n\n"+''.join(liste),'\n\n'+'\n'.join(liste_mauvais_mots),'\033[0;0m')
         mot = input('\n')
-        if mot.lower() == bon_mot:
+        if mot.upper() == bon_mot:
             print('\033[1;37mMot trouvé !\033[0;0m')
             break
         elif len(mot) < len(liste_bon_mot):
@@ -37,11 +37,12 @@ def jeu_wordle():
             trop = True
             continue
         else:
+            liste_mauvais_mots.append(mot.upper())
             nb_tentatives -= 1
             liste_mot = [i for i in mot]
             for i in range(len(liste_bon_mot)):
-                if liste_mot[i].lower() == liste_bon_mot[i].lower():
-                    liste[i] = liste_mot[i].lower()
+                if liste_mot[i].upper() == liste_bon_mot[i].upper():
+                    liste[i] = liste_mot[i].upper()
             os.system('clear')
             continue
     if nb_tentatives == 0:
