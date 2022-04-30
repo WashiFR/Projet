f691c424de381e2c3b00bc8d3c22b30d4dd1a5e5
Washi <50027934+WashiFR@users.noreply.github.com>
2022-04-30 12:05:34 +0200

    Update pfc.py

--- pfc.py
+++ pfc.py
@@ -17,7 +17,7 @@ def jeu_pfc():
 | |   | |  __/ |  | | |  __/      | | |  __/ |_| | | | |  __/      | |____| \__ \  __/ (_| | |_| |>  < 
 |_|   |_|\___|_|  |_|  \___|      |_|  \___|\__,_|_|_|_|\___|       \_____|_|___/\___|\__,_|\__,_/_/\_\
 \033[0;0m\n"""'\n\033[1;37m--------------------------------------------------------------------\n\033[0;0m')
-        j1 = input("\033[1;37mJoueur 1 choisis :\n- Pierre (P)\n- Feuille (F)\n- Ciseaux (C)\n\033[0;0m")
+        j1 = input("\033[1;37mJoueur 1 choisis :\n- Pierre (P)\n- Feuille (F)\n- Ciseaux (C)\n\nEntré la lettre : \033[0;0m")
     os.system("clear")
     while j2 not in ["pierre","feuille","ciseaux","p","P","c","C","f","F"]:
         os.system('clear')
@@ -27,8 +27,8 @@ def jeu_pfc():
 |  ___/ |/ _ \ '__| '__/ _ \______|  __/ _ \ | | | | | |/ _ \______| |    | / __|/ _ \/ _` | | | \ \/ /
 | |   | |  __/ |  | | |  __/      | | |  __/ |_| | | | |  __/      | |____| \__ \  __/ (_| | |_| |>  < 
 |_|   |_|\___|_|  |_|  \___|      |_|  \___|\__,_|_|_|_|\___|       \_____|_|___/\___|\__,_|\__,_/_/\_\
-\033[0;0m\n"""'\n')
-        j2 = input("\033[1;37mJoueur 2 choisis :\n- Pierre (P)\n- Feuille (F)\n- Ciseaux (C)\n\033[0;0m")
+\033[0;0m\n"""'\n\033[1;37m--------------------------------------------------------------------\n\033[0;0m')
+        j2 = input("\033[1;37mJoueur 2 choisis :\n- Pierre (P)\n- Feuille (F)\n- Ciseaux (C)\n\nEntré la lettre : \033[0;0m")
     if j1 in pierre and j2 in feuille or j1 in feuille and j2 in ciseaux or j1 in ciseaux and j2 in pierre:
         print("\033[1;37m--------------------------------------------------------------------\nJ1 :",j1,"\n""J2 :",j2,"\n\n""Joueur 2 a gagné ! \033[0;0m")
     elif j2 in pierre and j1 in feuille or j2 in feuille and j1 in ciseaux or j2 in ciseaux and j1 in pierre:
@@ -59,7 +59,7 @@ def jeu_pfc_ia():
 | |   | |  __/ |  | | |  __/      | | |  __/ |_| | | | |  __/      | |____| \__ \  __/ (_| | |_| |>  < 
 |_|   |_|\___|_|  |_|  \___|      |_|  \___|\__,_|_|_|_|\___|       \_____|_|___/\___|\__,_|\__,_/_/\_\
 \033[0;0m\n"""'\n\033[1;37m--------------------------------------------------------------------\n\033[0;0m')
-        j1 = input("\033[1;37mJoueur 1 choisis :\n- Pierre (P)\n- Feuille (F)\n- Ciseaux (C)\n\033[0;0m")
+        j1 = input("\033[1;37mJoueur 1 choisis :\n- Pierre (P)\n- Feuille (F)\n- Ciseaux (C)\n\nEntré la lettre : \033[0;0m")
     os.system("clear")
     print('\033[1;34m'""" _____ _                           ______         _ _ _              _____ _                           
 |  __ (_)                         |  ____|       (_) | |            / ____(_)                          
